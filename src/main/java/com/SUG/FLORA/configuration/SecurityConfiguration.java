f8a2926e0f5440e062e6c8e3d47042e2fb89ab94
VG-Correa <104435149+VG-Correa@users.noreply.github.com>
2024-02-29 09:59:18 -0300

    ambiente configurado e pronto para desenvolvimento

---
+++ src/main/java/com/SUG/FLORA/configuration/SecurityConfiguration.java
@@ -0,0 +1,63 @@
+package com.SUG.FLORA.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfiguration {
+    
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+            .password(passwordEncoder().encode("123"))
+            .roles("USER")
+            .build();
+        UserDetails user2 = User.withUsername("user2")
+            .password(passwordEncoder().encode("123"))
+            .roles("USER")
+            .build();
+        UserDetails admin = User.withUsername("admin")
+            .password(passwordEncoder().encode("123"))
+            .roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf( t -> t.disable());
+
+        http
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/css/**", "/images/**", "/js/**").permitAll()
+            )
+            .formLogin((formLogin) ->
+            formLogin
+                .loginPage("/login")
+                .permitAll()
+            
+        ); 
+
+            return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
