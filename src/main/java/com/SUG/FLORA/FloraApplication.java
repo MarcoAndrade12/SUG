bbb5a6240a5ccc090e5acf4697ba4c961dcf7254
MarcoAndrade12 <103834969+MarcoAndrade12@users.noreply.github.com>
2024-04-25 12:31:44 -0300

    Update FloraApplication.java

--- src/main/java/com/SUG/FLORA/FloraApplication.java
+++ src/main/java/com/SUG/FLORA/FloraApplication.java
@@ -1,6 +1,5 @@
 package com.SUG.FLORA;
 
-import com.SUG.FLORA.DatabaseConnection; // Corrigindo o import para a classe DatabaseConnection
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FloraApplication {
 
     public static void main(String[] args) {
-        // Inicializa a conexão com o banco de dados antes de iniciar a aplicação Spring Boot
-        DatabaseConnection.main(args);
+   
 
         // Inicia a aplicação Spring Boot
         SpringApplication.run(FloraApplication.class, args);
